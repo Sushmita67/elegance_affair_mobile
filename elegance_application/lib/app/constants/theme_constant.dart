06110d70f00a0d9481a4f66843f252ec98afe982
Sushmita67 <biswakarmasushmita5@gmail.com>
2025-01-17 00:56:50 +0545

    Added Files

---
+++ elegance_application/lib/app/constants/theme_constant.dart
@@ -0,0 +1,8 @@
+
+class ThemeConstant {
+  ThemeConstant._();
+
+  // static const Color darkPrimaryColor = Colors.yellow;
+  // static const Color primaryColor = Colors.green;
+  // static const Color appBarColor = Colors.green;
+}
