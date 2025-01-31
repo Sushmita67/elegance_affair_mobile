720c1d23e31c6a7a9f168a2474052f989e4adbcd
Sushmita67 <biswakarmasushmita5@gmail.com>
2025-01-31 14:09:44 +0545

    Updated Splash

--- elegance_application/lib/features/splash/presentation/view_model/splash_cubit.dart
+++ elegance_application/lib/features/splash/presentation/view_model/splash_cubit.dart
@@ -3,21 +3,50 @@ import 'package:elegance_application/features/onboarding/presentation/view_model
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class SplashCubit extends Cubit<void> {
-  SplashCubit(this._onboardingCubit) : super(null);
+import '../../../../app/di/di.dart';
 
-  final OnboardingCubit _onboardingCubit;
+// class SplashCubit extends Cubit<void> {
+//   SplashCubit(this._onboardingCubit) : super(null);
+
+//   final OnboardingCubit _onboardingCubit;
+//
+//   Future<void> init(BuildContext context) async {
+//     await Future.delayed(const Duration(seconds: 3), () async {
+//       if (context.mounted) {
+//         Navigator.pushReplacement(
+//           context,
+//           MaterialPageRoute(
+//             builder: (context) => BlocProvider.value(
+//               value: _onboardingCubit,
+//               child: OnboardingScreenView(),
+//             ),
+//           ),
+//         );
+//       }
+//     });
+//   }
+// }
+
+
+class SplashCubit extends Cubit<void> {
+  SplashCubit() : super(null);
 
   Future<void> init(BuildContext context) async {
-    await Future.delayed(const Duration(seconds: 3), () async {
+    await Future.delayed(const Duration(seconds: 2), () async {
+      // Ensure context is mounted before navigating
       if (context.mounted) {
+        // Navigate to OnboardingView wrapped in BlocProvider
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(
-            builder: (context) => BlocProvider.value(
-              value: _onboardingCubit,
-              child: OnboardingScreenView(),
-            ),
+            builder: (context) {
+              // Provide OnboardingCubit using getIt
+              return BlocProvider<OnboardingCubit>(
+                create: (_) => getIt<OnboardingCubit>(),
+                // Ensure OnboardingCubit is available
+                child: OnboardingView(), // Navigate to OnboardingView
+              );
+            },
           ),
         );
       }
