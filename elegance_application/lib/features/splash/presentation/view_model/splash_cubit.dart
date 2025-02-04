86a03e5add859a511547c0c5c8ec8be1b7709cb8
Sushmita67 <biswakarmasushmita5@gmail.com>
2025-02-04 13:33:00 +0545

    Update splash_cubit.dart

--- elegance_application/lib/features/splash/presentation/view_model/splash_cubit.dart
+++ elegance_application/lib/features/splash/presentation/view_model/splash_cubit.dart
@@ -32,7 +32,7 @@ class SplashCubit extends Cubit<void> {
   SplashCubit() : super(null);
 
   Future<void> init(BuildContext context) async {
-    await Future.delayed(const Duration(seconds: 2), () async {
+    await Future.delayed(const Duration(seconds: 4), () async {
       // Ensure context is mounted before navigating
       if (context.mounted) {
         // Navigate to OnboardingView wrapped in BlocProvider
