327ca3c6ec3111267de3d4e420a27c69e4aea4a4
Sushmita67 <biswakarmasushmita5@gmail.com>
2025-02-07 12:31:43 +0545

    token and mock for testing

---
+++ elegance_application/test/features/auth/domain/use_case/auth_repo.mock.dart
@@ -0,0 +1,4 @@
+import 'package:elegance_application/features/auth/domain/repository/user_repository.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockAuthRepository extends Mock implements IUserRepository {}
